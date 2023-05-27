e0087ed5f172bcda2fc7d694fdc8daf4c1b72fa1
TrungBui59 <bui23@purdue.edu>
2023-05-27 16:39:17 -0700

    Finish second problem

---
+++ sol-1907.sql
@@ -0,0 +1,8 @@
+select 'Low Salary' as category , count( account_id ) as accounts_count from Accounts
+where account_id in ( select account_id from accounts where income < 20000 ) 
+union
+select 'Average Salary' as category , count( account_id ) as accounts_count from Accounts
+where account_id in ( select account_id from accounts where income >= 20000 and income <= 50000 )
+union 
+select 'High Salary' as category , count( account_id ) as accounts_count from Accounts
+where account_id in ( select account_id from accounts where income > 50000 )
